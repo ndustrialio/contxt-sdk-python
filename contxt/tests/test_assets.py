a16a912acab83a64a5681b9672a362c77b85bae0
Branch Vincent <branchevincent@gmail.com>
2019-03-05 16:49:34 -0500

    Update tests

--- contxt/tests/test_assets.py
+++ contxt/tests/test_assets.py
@@ -3,7 +3,7 @@ import pytest
 from contxt.asset_framework_new import AssetFramework
 from contxt.services.asset_models import (Asset, AssetType, Attribute,
                                           AttributeValue, DataTypes, Metric,
-                                          MetricValue)
+                                          MetricValue, TimeIntervals)
 from contxt.utils.auth import CLIAuth
 
 
@@ -15,7 +15,7 @@ def cli_auth():
 @pytest.fixture
 def asset_framework(cli_auth: CLIAuth,
                     organization_id="02efa741-a96f-4124-a463-ae13a704b8fc"):
-    return AssetFramework(cli_auth, organization_id, env="staging", load_all_types=False)
+    return AssetFramework(cli_auth, organization_id, env="staging", load_types=False)
 
 @pytest.fixture
 def asset_type(asset_framework):
@@ -25,7 +25,12 @@ def asset_type(asset_framework):
         organization_id=asset_framework.organization_id)
     return asset_framework.create_asset_type(asset_type)
 
+@pytest.fixture
+def asset(asset_framework):
+    asset = Asset()
 
+
+# TODO: need to split these tests up, and test for errors, globals, different orgs
 class TestAssetFramework:
 
     def test_asset_type_crud_endpoints(self, asset_framework: AssetFramework):
@@ -149,10 +154,96 @@ class TestAssetFramework:
         asset_framework.delete_asset_type(asset_type)
 
     def test_attribute_value_crud_endpoints(self, asset_framework, asset_type):
-        pass
+        # Create metric
+        metric = Metric(
+            asset_type_id=asset_type.id,
+            label="test_label",
+            description="Test description",
+            organization_id=af.organization_id,
+            time_interval=TimeIntervals.daily,
+            units="?")
+        created_metric = af.create_metric(metric)
+
+        # Test create_metric_value
+        metric_value = MetricValue(
+            asset_id="?",
+            asset_metric_id=created_metric.id,
+            effective_start_date="?",
+            effective_end_date="?",
+            notes="Test note",
+            value=5)
+        created_metric_value = asset_framework.create_metric_value(metric_value)
+        assert created_metric_value.asset_id == metric_value.asset_id
+        assert created_metric_value.asset_metric_id == metric_value.asset_metric_id
+        assert created_metric_value.effective_start_date == metric_value.effective_start_date
+        assert created_metric_value.effective_end_date == metric_value.effective_end_date
+        assert created_metric_value.notes == metric_value.notes
+        assert created_metric_value.value == metric_value.value
+
+        # Test update_metric_value and get_metric_value
+        asset_framework.update_metric_value(created_metric_value)
+        updated_metric_value = asset_framework.get_metric_value(
+            created_metric_value.id)
+
+        # Test delete_metric_value
+        asset_framework.delete_metric_value(updated_metric_value)
+        # Delete the test asset_type
+        asset_framework.delete_asset_type(asset_type)
 
     def test_metric_crud_endpoints(self, asset_framework, asset_type):
-        pass
-    
+        # Test create_metric
+        metric = Metric(
+            asset_type_id=asset_type.id,
+            label="test_label",
+            description="Test description",
+            organization_id=asset_framework.organization_id,
+            time_interval=TimeIntervals.daily,
+            units="?")
+        created_metric = asset_framework.create_metric(metric)
+        # Check created metric has the same fields
+        assert created_metric.asset_type_id == metric.asset_type_id
+        assert created_metric.label == metric.label
+        assert created_metric.description == metric.description
+        assert created_metric.organization_id == metric.organization_id
+        assert created_metric.time_interval == metric.time_interval
+        assert created_metric.units == metric.units
+
+        # Test update_metric and get_metric
+        created_metric.label = "edited_test_label"
+        created_metric.description = "edited test description"
+        created_metric.time_interval = TimeIntervals.weekly
+        created_metric.units = "??"
+        asset_framework.update_metric(created_metric)
+        updated_metric = asset_framework.get_metric(created_metric.id)
+        assert updated_metric.label == created_metric.label
+        assert updated_metric.description == created_metric.description
+        assert updated_metric.time_interval == created_metric.time_interval
+        assert updated_metric.units == created_metric.units
+
+        # Test delete_metric
+        asset_framework.delete_metric(updated_metric)
+        # Check the asset was actually deleted
+        with pytest.raises(Exception) as e:
+            asset_framework.get_asset(updated_metric.id)
+
+        # Delete the test asset_type
+        asset_framework.delete_asset_type(asset_type)
+
     def test_metric_value_crud_endpoints(self, asset_framework, asset_type):
-        pass
+        # Delete the test asset_type
+        asset_framework.delete_asset_type(asset_type)
+
+
+if __name__ == "__main__":
+    auth = CLIAuth()
+    organization_id = "02efa741-a96f-4124-a463-ae13a704b8fc"
+    asset_framework = AssetFramework(
+        auth,
+        organization_id,
+        env='staging',
+        load_types=True,
+        types_to_fully_load=['UtilityMeter'])
+    af = asset_framework
+    asset_type = af.asset_type_with_label('UtilityMeter')
+
+    TestAssetFramework().test_attribute_value_crud_endpoints(asset_framework, asset_type)
