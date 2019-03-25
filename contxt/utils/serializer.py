28b69d50395c50b82fb13c6cec3ed0fc623cd37b
Branch Vincent <branchevincent@gmail.com>
2019-03-25 16:05:14 -0400

    Handle to_dict returning lists in some cases

--- contxt/utils/serializer.py
+++ contxt/utils/serializer.py
@@ -31,6 +31,10 @@ class Serializer:
         :return: dictionary
         :rtype: `dict`
         """
+        # TODO: this may not return a dict but instead a list, or a native type
+        # (for example, if passed an int, it will return it). this is likely
+        # an unexpected behavior for callers
+
         # Create default key filter (ignore _ vars)
         if key_filter is None:
             key_filter = lambda k: not k.startswith("_")
@@ -56,14 +60,14 @@ class Serializer:
             return [Serializer.to_dict(v, cls_key) for v in obj]
         elif hasattr(obj, "__dict__"):
             # General object
-            data = {
+            d = {
                 k: Serializer.to_dict(v, cls_key)
                 for k, v in obj.__dict__.items()
                 if not callable(v) and key_filter(k)
             }
             if cls_key is not None and hasattr(obj, "__class__"):
-                data[cls_key] = obj.__class__.__name__
-            return data
+                d[cls_key] = obj.__class__.__name__
+            return d
         else:
             # Fallback to self
             return obj
@@ -102,7 +106,12 @@ class Serializer:
         """
 
         d = Serializer.to_dict(obj)
-        return tabulate([d.values()], headers="keys", **kwargs)
+
+        # TODO: The above may be a list, so handle it here
+        if not isinstance(d, (list, tuple)):
+            d = [d]
+
+        return tabulate(d, headers="keys", **kwargs)
 
     @staticmethod
     def to_csv(obj: Any, path: Path, header: bool = True, **kwargs):
@@ -115,13 +124,30 @@ class Serializer:
         :param header: write header row, defaults to True
         :param header: bool, optional
         """
+        def keys(o):
+            if isinstance(o, dict):
+                return o.keys()
+            elif isinstance(o, (list, tuple)) and o:
+                # TODO: in this case, we assume the entire list has identical keys
+                return keys(o[0])
+            else:
+                return []
 
+        # Get dict
         d = Serializer.to_dict(obj)
+
+        # Write
         with path.open("w") as f:
-            writer = DictWriter(f, fieldnames=d.keys(), **kwargs)
+            writer = DictWriter(f, fieldnames=keys(d), **kwargs)
             if header:
                 writer.writeheader()
-            writer.writerows(d)
+            if isinstance(d, (list, tuple)):
+                # Write list
+                writer.writerows(d)
+            else:
+                # TODO: may not actually be a dict
+                # Write dict
+                writer.writerow(d)
 
 
 if __name__ == "__main__":
