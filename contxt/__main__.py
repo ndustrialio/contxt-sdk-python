cb50d5b747a1ac3ce4411b0f78f54271c41472fa
Branch Vincent <branchevincent@gmail.com>
2019-03-26 11:17:06 -0400

    Add version to cli

--- contxt/__main__.py
+++ contxt/__main__.py
@@ -5,14 +5,19 @@ from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 
 from argcomplete import autocomplete
 
+from contxt.__version__ import __version__
 from contxt.cli.parsers import ArgParser
 
 
 def main():
-    # Setup parser and our subparsers, which are subclassed from ArgParser
+    # Setup parser
     root_parser = ArgumentParser(
         description="Contxt CLI",
         formatter_class=ArgumentDefaultsHelpFormatter)
+    root_parser.add_argument(
+        "-v", "--version", action="version", version=f"%(prog)s {__version__}")
+
+    # Setup our subparsers, which are subclassed from ArgParser
     parsers = root_parser.add_subparsers(title="subcommands", dest="command")
     subparsers = [cls(parsers) for cls in ArgParser.__subclasses__()]
 
