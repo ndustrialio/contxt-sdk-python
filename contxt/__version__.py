d04db9752090012711c2de9a93eaf7eeeaecbde7
Branch Vincent <branchevincent@gmail.com>
2019-03-26 11:15:10 -0400

    Add __version__ file

---
+++ contxt/__version__.py
@@ -0,0 +1,7 @@
+__title__ = "contxt-sdk"
+__description__ = "Contxt SDK from ndustrial.io"
+__url__ = "https://github.com/ndustrialio/contxt-sdk-python"
+__version__ = "0.0.1b1"
+__author__ = "ndustrial.io"
+__author_email__ = "dev@ndustrial.io"
+__license__ = "ISC"
