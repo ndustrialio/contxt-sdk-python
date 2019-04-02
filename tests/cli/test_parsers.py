dbf38cb7dbd271eb0cc9c4f69162138f75d1093b
Bret Askeland <baskeland@ndustrial.com>
2019-04-02 15:22:40 -0400

    updates

--- tests/cli/test_parsers.py
+++ tests/cli/test_parsers.py
@@ -3,16 +3,17 @@ import contxt.__main__ as contxt
 import pytest
 
 @pytest.fixture()
-def setup_parser():
-    return contxt.create_parser()
+def setup_parser_args():
+    parser = contxt.create_parser()
+    return parser.parse_args()
 
-def test_create_parser(setup_parser):
-    args = setup_parser.parse_args()
-    if not args:
+def test_create_parser(setup_parser_args):
+    if not setup_parser_args:
         pytest.fail("No args exist for parser")
 
-def test_facility_parser(setup_parser):
+def test_facility_parser(setup_parser_args):
     pass
-
-def test_iot_parser(setup_parser):
+    
+    
+def test_iot_parser(setup_parser_args):
     pass
