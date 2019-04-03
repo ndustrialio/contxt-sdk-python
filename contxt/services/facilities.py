e2905d35256597cb854c5af936b20e9419b1db2c
Branch Vincent <branchevincent@gmail.com>
2019-04-03 13:44:14 -0400

    Modernize facilities service

--- contxt/services/facilities.py
+++ contxt/services/facilities.py
@@ -1,83 +1,52 @@
-from contxt.legacy.services import GET, APIObject, APIObjectCollection, Service
-from contxt.services.contxt import Organization
+from typing import Any, Callable, Dict, List, Optional, Set, Tuple
 
-CONFIGS_BY_ENVIRONMENT = {
-    'production': {
-        'base_url': 'https://facilities.api.ndustrial.io/',
-        'audience': 'SgbCopArnGMa9PsRlCVUCVRwxocntlg0'
-    },
-    'staging': {
-        'base_url': 'https://facilities-staging.api.ndustrial.io/',
-        'audience': 'xG775XHIOZVUn84seNeHXi0Qe55YuR5w'
-    }
-}
+from contxt.auth.cli import CLIAuth
+from contxt.models.facilities import Facility
+from contxt.services.api import ConfiguredApiService
+from contxt.services.assets import AssetsService
+from contxt.utils import make_logger
+
+logger = make_logger(__name__)
 
 
-class FacilitiesService(Service):
+class FacilitiesService(ConfiguredApiService):
+    """
+    Service to interact with our Facilities api.
 
-    def __init__(self, auth_module, environment='production'):
+    NOTE: The facility_id in this service is the legacy integer id.
+    """
+    _configs = AssetsService._configs
 
-        if environment not in CONFIGS_BY_ENVIRONMENT:
-            raise Exception('Invalid environment specified')
+    def __init__(self, auth: CLIAuth, env: str = "production"):
+        super().__init__(auth, env)
 
-        self.env = CONFIGS_BY_ENVIRONMENT[environment]
+    def get_facilities(self, organization_id: Optional[str] = None):
+        logger.debug(f"Fetching facilities for organization {organization_id}")
+        uri = f"organizations/{organization_id}/facilities" if organization_id is not None else "facilities"
+        resp = self.get(uri)
+        # TODO: handle not found errors here, and return None instead of raising an error
+        return [Facility.from_api(rec) for rec in resp]
 
-        super().__init__(
-            base_url=self.env['base_url'],
-            access_token=auth_module.get_token_for_audience(
-                self.env['audience']))
+    def get_facility_by_id(self, facility_id: int):
+        logger.debug(f"Fetching facility {facility_id}")
+        resp = self.get(f"facilities/{facility_id}")
+        # TODO: handle not found errors here, and return None instead of raising an error
+        return Facility.from_api(resp)
 
-    def get_facilities(self, organization_id=None):
+    def get_facility_by_name(self, name: str, organization_id: Optional[str] = None):
+        logger.debug(f"Fetching facility {name}")
+        facilities = self.get_facilities(organization_id=organization_id)
+        for facility in facilities:
+            if facility.name.lower() == name.lower():
+                return facility
+        logger.warning(f"Failed to find facility with name {name}")
 
-        uri = 'facilities'
-
-        if organization_id:
-            assert isinstance(organization_id, str)
-            uri = f'organizations/{organization_id}/facilities'
-
-        response = self.execute(GET(uri=uri))
-
-        facilities = []
-        for org in response:
-            facilities.append(Facility(org))
-
-        return APIObjectCollection(facilities)
-
-    def get_facility_by_id(self, facility_id):
-
-        assert isinstance(facility_id, int)
-
-        return Facility(self.execute(GET(uri=f'facilities/{facility_id}')))
-
-
-class Facility(APIObject):
-
-    def __init__(self, facility_api_object, keys_to_ignore=None):
-
-        super().__init__(
-            keys_to_ignore=keys_to_ignore if keys_to_ignore is not None else [
-                'address1', 'address2', 'geometry_id', 'tags',
-                'Organization', 'Info'
-            ])
-
-        self.id = facility_api_object['id']
-        self.name = facility_api_object['name']
-        self.address1 = facility_api_object['address1']
-        self.address2 = facility_api_object['address2']
-        self.city = facility_api_object['city']
-        self.state = facility_api_object['state']
-        self.zip = facility_api_object['zip']
-        self.timezone = facility_api_object['timezone']
-        self.geometry_id = facility_api_object['geometry_id']
-        self.asset_id = facility_api_object['asset_id']
-        self.tags = facility_api_object['tags']
-        self.organization_id = facility_api_object['organization_id']
-        self.Organization = Organization(facility_api_object['Organization'])
-        self.created_at = facility_api_object['created_at']
-        self.Info = facility_api_object['Info']
-
-    def get_dict(self):
-        return {
-            **super().get_dict(),
-            'organization_name': self.Organization.name
-        }
+    def get_facility_by_asset_id(self,
+                                 asset_id: str,
+                                 organization_id: Optional[str] = None):
+        logger.debug(f"Fetching facility {asset_id}")
+        facilities = self.get_facilities(organization_id=organization_id)
+        for facility in facilities:
+            if facility.asset_id == asset_id:
+                return facility
+        logger.warning(f"Failed to find facility with asset_id {asset_id}")
