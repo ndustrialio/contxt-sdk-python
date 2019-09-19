6cb586771f34ce31f72f26e3b1a5753e065f627b
Branch Vincent <branchevincent@gmail.com>
2019-09-19 12:06:26 -0400

    Revert debug tweaks to test

--- tests/integration/test_base_api.py
+++ tests/integration/test_base_api.py
@@ -11,20 +11,13 @@ class TestBaseApi:
         retry = ApiRetry()
         api = Api("http://httpbin.org", retry=retry)
         t0 = time()
-        try:
+        with pytest.raises(RetryError):
             api.get("status/500")
-        except RetryError as e:
-            pass
         t = time() - t0
         assert t >= sum(retry.backoff_factor * (2 ** n) for n in range(retry.total))
 
     def test_without_retires(self):
         api = Api("http://httpbin.org", retry=None)
-        try:
+        with pytest.raises(HTTPError) as e:
             api.get("status/500")
-        except HTTPError as e:
-            pass
-
-
-TestBaseApi().test_retries()
-TestBaseApi().test_without_retires()
+            assert e.status == 501
