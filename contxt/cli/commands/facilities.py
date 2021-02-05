6cf45134c302cdbaa3d85696e2160f9e28d53a76
Branch Vincent <branchevincent@gmail.com>
2021-02-05 11:37:19 -0500

    feat: add ability to create facilities (#109)
    [ci skip]

--- contxt/cli/commands/facilities.py
+++ contxt/cli/commands/facilities.py
@@ -1,4 +1,5 @@
-from typing import List
+from csv import DictReader
+from typing import IO, List
 
 import click
 
@@ -20,3 +21,28 @@ def get(clients: Clients, fields: List[str], sort: str) -> None:
     """Get facilities"""
     items = clients.facilities.get_facilities()
     print_table(items=items, keys=fields, sort_by=sort)
+
+
+@facilities.command()
+@click.argument("org_id")
+@click.option("--input", required=True, type=click.File(), help="CSV of facilities to create")
+@click.pass_obj
+def create(clients: Clients, org_id: str, input: IO[str]) -> None:
+    """Create facilities"""
+    # Parse file
+    try:
+        facilities = [
+            dict(name=r["name"], timezone=r["timezone"], organization_id=org_id)
+            for r in DictReader(input)
+        ]
+    except KeyError:
+        raise click.ClickException("The following columns are required: name, timezone")
+
+    # Create facilities
+    with click.progressbar(
+        facilities,
+        label="Creating facilities",
+        item_show_func=lambda f: f"Facility {f['name']}" if f else "",
+    ) as facilities_:
+        for f in facilities_:
+            clients.facilities.post("facilities", data=f)
