fee0be733e93a58ba16805844d884ddc4e0dc23f
Zack Behringer <zack@ndustrial.io>
2022-02-23 14:20:45 -0500

    MOB-1094 DLQ consumer framework
    will be hooked in to the WMS ETL CLI

---
+++ contxt/dlq_consumer.py
@@ -0,0 +1,127 @@
+import csv
+import json
+import logging
+from contextlib import closing
+from typing import TextIO
+
+import websocket
+from requests.exceptions import HTTPError
+
+from contxt.message_bus import MessageBus
+from contxt.services.api import Api
+
+from .auth.machine import MachineAuth
+
+logger = logging.getLogger()
+
+
+class DlqConsumer:
+    def __init__(
+        self,
+        client_id: str,
+        client_secret: str,
+        base_socket_url: str,
+        org_id: str,
+        etl_mgmt_api_base_url: str,
+        etl_mgmt_api_client_id: str,
+        channel: str,
+        subscription_name: str,
+        acknowledge: bool,
+        receive_timeout=5,
+    ) -> None:
+        self.auth = MachineAuth(client_id, client_secret)
+        self.base_socket_url = base_socket_url
+        self.org_id = org_id
+        self.etl_mgmt_api_base_url = etl_mgmt_api_base_url
+        self.etl_mgmt_api_client_id = etl_mgmt_api_client_id
+        self.channel = channel
+        self.subscription_name = subscription_name
+        self.acknowledge = acknowledge
+        self.receive_timeout = receive_timeout
+
+    def pullData(self, dst: TextIO, fields, headers=True, limit=0):
+        bearer_token = self.auth.get_token_provider(self.etl_mgmt_api_client_id).access_token
+
+        channelAuthApi = Api(self.etl_mgmt_api_base_url)
+        channel_token_resp = channelAuthApi.raw_post(
+            "v1/channels/authorizations",
+            headers={"Authorization": "Bearer " + bearer_token},
+            json={"resources": [{"resource": self.channel, "actions": ["subscribe"]}]},
+        )
+        try:
+            # Raise any error
+            channel_token_resp.raise_for_status()
+        except HTTPError:
+            # Catch the error, to log the response's message, and reraise
+            # Try to decode the response as json, else fall back to raw text
+            response_json = channel_token_resp.json()
+            msg = response_json.get("message") or response_json or channel_token_resp.text
+            logger.debug(f"HTTP Error: {channel_token_resp.reason} - {msg}")
+            raise
+
+        socketURL = self.base_socket_url + "/organizations/" + self.org_id + "/stream"
+
+        logger.debug(f"connecting to {socketURL}")
+        websocket.enableTrace(logger.level <= 10)
+        # connect to message bus websocket
+        with closing(
+            websocket.create_connection(socketURL, header={"Authorization": "Bearer " + bearer_token})
+        ) as ws:
+
+            logger.info("connected to message bus")
+            ws.settimeout(self.receive_timeout)
+
+            mb = MessageBus(ws)
+            if not mb.authorize(channel_token_resp.text):
+                return
+            if not mb.subscribe(self.etl_mgmt_api_client_id, self.channel, self.subscription_name):
+                return
+
+            # set timeout for receiving in order to detect end of stream
+            ws.settimeout(self.receive_timeout)
+
+            logger.info("streaming DLQ data")
+
+            queue_message = None
+            if fields is None or len(fields) == 0:
+                # infer the fields from the first message
+                logger.debug("waiting for DLQ row from message bus")
+                try:
+                    queue_message = json.loads(ws.recv())
+                except websocket.WebSocketTimeoutException:
+                    logger.info("timeout waiting for first DLQ row from message bus. Closing connection")
+                    # end of queue if timeout received?
+                    return
+
+                fields = queue_message.keys()
+
+            writer = csv.DictWriter(dst, fieldnames=fields)
+            writer.writeheader()
+            count = 0
+
+            if queue_message is not None:
+                logger.debug(f"writing DLQ row {queue_message}")
+                writer.writerow(queue_message)
+                count += 1
+
+                if self.acknowledge:
+                    logger.debug(f"acknowledging DLQ row {queue_message.message_id}")
+                    mb.acknowledge(queue_message.message_id)
+
+            while limit == 0 or count < limit:
+                queue_message = None
+                logger.debug("waiting for DLQ row from message bus")
+                try:
+                    queue_message = json.loads(ws.recv())
+                except websocket.WebSocketTimeoutException:
+                    logger.info("timeout waiting for next DLQ row from message bus. Closing connection")
+                    # end of queue if timeout received?
+                    break
+
+                logger.debug(f"writing DLQ row {queue_message}")
+                writer.writerow(queue_message)
+                count += 1
+
+                if self.acknowledge:
+                    logger.debug(f"acknowledging DLQ row {queue_message.message_id}")
+                    mb.acknowledge(queue_message.message_id)
---
+++ contxt/message_bus.py
@@ -0,0 +1,80 @@
+import json
+import logging
+import uuid
+
+import websocket
+
+logger = logging.getLogger()
+
+
+class MessageBus:
+    def __init__(self, ws: websocket.WebSocket) -> None:
+        self.ws = ws
+
+    def authorize(self, token: str):
+        logger.debug("sending authorization to message bus")
+        self.ws.send(
+            json.dumps(
+                {
+                    "jsonrpc": "2.0",
+                    "method": "MessageBus.Authorize",
+                    "params": {"token": token},
+                    "id": str(uuid.uuid4()),
+                }
+            )
+        )
+
+        # receive data
+        logger.debug("waiting for message bus authorization response")
+        try:
+            response = json.loads(self.ws.recv())
+        except websocket.WebSocketTimeoutException:
+            logger.warn("Timeout waiting for message bus authorization response")
+            return False
+        except Exception as e:
+            logger.error("Error waiting for authorization response", e)
+            return False
+        logger.debug(f"message bus authorization response ${response}")
+        return True
+
+    def subscribe(self, service_client_id: str, channel: str, group: str):
+        logger.debug(f"sending channel {channel} subscription to message bus")
+        self.ws.send(
+            json.dumps(
+                {
+                    "jsonrpc": "2.0",
+                    "method": "MessageBus.Subscribe",
+                    "params": {
+                        "service_id": service_client_id,
+                        "channel": channel,
+                        "group": group,
+                    },
+                    "id": str(uuid.uuid4()),
+                }
+            )
+        )
+
+        logger.debug(f"waiting for channel {channel} subscription response")
+        try:
+            response = json.loads(self.ws.recv())
+        except websocket.WebSocketTimeoutException:
+            logger.warn(f"Timeout waiting for channel {channel} subscription response")
+            return False
+        except Exception as e:
+            logger.error(f"Error waiting for channel {channel} subscription response", e)
+            return False
+        logger.debug(f"channel {channel} subscription response: {response}")
+        return True
+
+    def acknowledge(self, msg_id: str):
+        ackId = uuid.uuid4()
+        self.ws.send(
+            json.dumps(
+                {
+                    "jsonrpc": "2.0",
+                    "method": "MessageBus.Acknowledge",
+                    "message_id": msg_id,
+                    "id": str(ackId),
+                }
+            )
+        )
