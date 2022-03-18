91198dc868d9bb9986a6d246944a4ba95b3c4893
Tripp Whaley <twhaley215@gmail.com>
2022-03-18 13:58:02 -0400

    allow slug to be passed in, may want to revert this

--- contxt/cli/commands/facilities.py
+++ contxt/cli/commands/facilities.py
@@ -14,18 +14,20 @@ def facilities() -> None:
 
 @facilities.command()
 @click.option("--org-id", help="Organization ID, defaults to first value in token if not specified")
+@click.option("--slug", help="Organization slug, overrides org-id if both are set")
 @click.pass_obj
-def get(clients: Clients, org_id: Optional[str] = None) -> None:
+def get(clients: Clients, org_id: Optional[str] = None, slug: Optional[str] = None) -> None:
     """Get facilities"""
-    facilities = clients.nionic.get_facilities(org_id).nodes
+    facilities = clients.nionic.get_facilities(org_id, slug).nodes
     print(Serializer.to_table(facilities))
 
 
 @facilities.command()
-@click.option("--org-id", required=True, help="Organization ID")
+@click.option("--org-id", help="Organization ID, defaults to first value in token if none specified")
+@click.option("--slug", help="Organization slug, overrides org-id if set")
 @click.option("--input", required=True, type=click.File(), help="CSV of facilities to create")
 @click.pass_obj
-def create(clients: Clients, org_id: str, input: IO[str]) -> None:
+def create(clients: Clients, input: IO[str], org_id: Optional[str] = None, slug: Optional[str] = None) -> None:
     """Create facilities from a CSV file, containing the columns:
 
     \b
@@ -47,4 +49,4 @@ def create(clients: Clients, org_id: str, input: IO[str]) -> None:
         item_show_func=lambda f: f"Facility {f['name']}" if f else "",
     ) as facilities_:
         for f in facilities_:
-            clients.nionic.create_facility(data=f, organization_id=org_id)
+            clients.nionic.create_facility(data=f, organization_id=org_id, slug = slug)
--- contxt/cli/commands/metrics.py
+++ contxt/cli/commands/metrics.py
@@ -13,15 +13,17 @@ def metrics() -> None:
 
 @metrics.command()
 @click.option("--org-id", help="Organization ID, defaults to first value in token if not specified")
+@click.option("--slug", help="Organization slug, overrides org-id if both are set")
 @click.pass_obj
-def labels(clients: Clients, org_id: Optional[str] = None) -> None:
+def labels(clients: Clients, org_id: Optional[str] = None, slug: Optional[str] = None) -> None:
     """Get Metric Labels"""
-    labels = clients.nionic.get_metric_labels(org_id)
+    labels = clients.nionic.get_metric_labels(org_id, slug)
     print(Serializer.to_table(labels))
 
 
 @metrics.command()
 @click.option("--org-id", help="Organization ID, defaults to first value in token if not specified")
+@click.option("--slug", help="Organization slug, overrides org-id if both are set")
 @click.option(
     "--source-id",
     required=True,
@@ -31,7 +33,7 @@ def labels(clients: Clients, org_id: Optional[str] = None) -> None:
     "--label", required=True, help="Organization ID, defaults to first value in token if not specified"
 )
 @click.pass_obj
-def data(clients: Clients, source_id: str, label: str, org_id: Optional[str] = None) -> None:
+def data(clients: Clients, source_id: str, label: str, org_id: Optional[str] = None, slug: Optional[str] = None) -> None:
     """Get facilities"""
-    facilities = clients.nionic.get_metric_data(label, source_id, org_id).nodes
+    facilities = clients.nionic.get_metric_data(label, source_id, org_id, slug).nodes
     print(Serializer.to_table(facilities))
