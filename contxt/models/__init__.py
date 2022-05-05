d8a1baf15a2020ffd1f90672867a4f6956f527b7
Kurt Kikendall <kurt@ndustrial.io>
2022-05-05 11:55:13 -0400

    fix: Update to handle facilities request moving to nionic.

--- contxt/models/__init__.py
+++ contxt/models/__init__.py
@@ -24,7 +24,7 @@ class Parsers:
 
     @staticmethod
     def date(datestamp: str) -> _date:
-        return _date.fromisoformat(datestamp)
+        return _date.fromisoformat(datestamp[:10])
 
     @staticmethod
     def datetime(timestamp: str) -> _datetime:
@@ -157,7 +157,7 @@ class ApiObject(ABC):
         for field in cls._api_fields:  # type: ignore
             if field.api_key not in api_dict and not field.optional:
                 raise KeyError(
-                    f"Required API field '{field.api_key}' is missing from" f" response: {api_dict}"
+                    f"{cls.__module__}.{cls.__name__} : Required API field '{field.api_key}' is missing from" f" response: {api_dict}"
                 )
             clean_dict[field.attr_key] = cls.clean_api_value(
                 field=field, value=api_dict.pop(field.api_key, None)
