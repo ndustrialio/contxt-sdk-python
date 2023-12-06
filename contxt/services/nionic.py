75a2880e588339211ffeb03c6df0f462d3eb1d67
Zack Behringer <zack@ndustrial.io>
2023-12-06 12:49:42 -0500

    feat: move main services from EMS to nionic (#378)
    * switched to using Nionic to retrieve main services and its data
    * updated main services data queries to sum usage for a time for fields with the same name
    * updated main services data query to require time parameters
    * switched metric data query to go through Facility, now requiring time parameters
    * refactored GraphQL interface to use schema and code generation where possible
    * fixed EMS data export command

--- contxt/services/nionic.py
+++ contxt/services/nionic.py
@@ -1,12 +1,15 @@
-from typing import List
+from typing import Any, Iterable, List, Optional
 
 from sgqlc.endpoint.requests import RequestsEndpoint
-from sgqlc.operation import Operation
 
-from contxt.models.facilities import Facility, MetricData, MetricLabel, Mutation, Query
+from contxt.generated.nionic_queries import Operations as nionic_operations
+from contxt.generated.nionic_schema import Facility, MainService, MetricLabel
+from contxt.models.ems import ResourceType
+from contxt.models.iot import Window
 from contxt.services.api import ApiEnvironment, BaseGraphService
 
 from ..auth import Auth
+from .pagination import DataPoint, PagedGQLTimeSeries
 
 
 class NionicService(BaseGraphService):
@@ -31,23 +34,77 @@ class NionicService(BaseGraphService):
         self.endpoint = RequestsEndpoint(self.base_url.rstrip("/") + "/graphql", session=self.session)
 
     def get_facilities(self) -> List[Facility]:
-        op = Operation(Query)
-        op.facilities().nodes().__fields__(*list(Facility._ContainerTypeMeta__fields.keys()))
+        op = nionic_operations.query.facilities
         return (op + self.run(op)).facilities.nodes
 
     def create_facility(self, data: dict) -> Facility:
-        op = Operation(Mutation)
-        op.create_facility(input={"facility": data}).__fields__("facility")
-        return self.run(op)["data"]["createFacility"]["facility"]
+        op = nionic_operations.mutation.create_facility
+        return self.run(op, data)["data"]["createFacility"]["facility"]
 
     def get_metric_labels(self) -> List[MetricLabel]:
-        op = Operation(Query)
-        op.metric_labels().__fields__(*list(MetricLabel._ContainerTypeMeta__fields.keys()))
+        op = nionic_operations.query.metric_labels
         return self.run(op).get("data").get("metricLabels")
 
-    def get_metric_data(self, label: str, source_id: str) -> List[MetricData]:
-        op = Operation(Query)
-        fields = list(MetricData._ContainerTypeMeta__fields.keys())
-        fields.remove("id")
-        op.metric_data(source_id=source_id, label=label).nodes().__fields__(*fields)
-        return (op + self.run(op)).metric_data.nodes
+    def get_metric_data(self, label: str, facility_id: int, start: str) -> List[Any]:
+        query = """
+        query metricData($facilityId: Int!, $label: String!, $from: String!) {
+            facility(id: $facilityId) {
+                metricData(label: $label, from: $from) {
+                    nodes {
+                        time
+                        data
+                    }
+                }
+            }
+        }
+        """
+        resp = self.query(query, {"facilityId": int(facility_id), "label": label, "from": start})
+        return resp["facility"]["metricData"]["nodes"]
+
+    def get_main_services(
+        self, facility_id: int, resource_type: Optional[ResourceType] = None
+    ) -> List[MainService]:
+        op = nionic_operations.query.main_services
+        filterValues = {"facilityId": int(facility_id)}
+        return [
+            x
+            for x in (op + self.run(op, variables=filterValues)).main_services.nodes
+            if resource_type is None or x.type.lower() == resource_type.value
+        ]
+
+    def get_data_point_data(
+        self, name: str, data_source_name: str, start: str, end: str, window: Window, per_page: int
+    ) -> Iterable[DataPoint]:
+        return PagedGQLTimeSeries(
+            api=self,
+            query="""
+            query dataPointData($dataSourceName: String!, $name: String!
+                , $from: String!, $to: String!, $window: String!, $samplingWindow: String!
+                , $limit: Int!, $after: Cursor) {
+                dataPoint(dataSourceName: $dataSourceName, name: $name) {
+                    data(from: $from, to: $to, orderBy: TIME_DESC
+                    , window: $window, samplingWindow: $samplingWindow
+                    , first: $limit, after: $after) {
+                        nodes {
+                            time
+                            data
+                        }
+                        pageInfo {
+                            hasNextPage
+                            endCursor
+                        }
+                    }
+                }
+            }
+            """,
+            params={
+                "dataSourceName": data_source_name,
+                "name": name,
+                "from": start,
+                "to": end,
+                "window": f"{window.value} seconds",
+                # limit to 1 day if window is greater than 1 day
+                "samplingWindow": f"{min(window.value, 86400)} seconds",
+            },
+            per_page=per_page,
+        )
